97b7c85a0b0f7cbc7cad85241bfa894ce4e667f7
PepengMaluwag <dbzforever02@gmail.com>
2024-05-08 02:41:41 -0700

    Ano daw dedebug namin? -Ryan

--- Assets/Scripts/ScaleBlueprint.cs
+++ Assets/Scripts/ScaleBlueprint.cs
@@ -4,16 +4,11 @@ using UnityEngine;
 
 public class ScaleBlueprint : MonoBehaviour
 {
-    private Vector3 bigscale, smallscale, originalpos, bigpos;
-    private bool isBig;
+    public Vector3 bigscale, smallscale, originalpos, bigpos;
+    public bool isBig;
     // Start is called before the first frame update
     void Start()
-    {
-        bigscale = new Vector3 (4, 4, 4);
-        smallscale = new Vector3(1, 1, 1);
-        originalpos = new Vector3(-5, 2, 0);
-        bigpos = new Vector3(0, 1, 0);
-        
+    {       
         isBig = false;
     }
 
